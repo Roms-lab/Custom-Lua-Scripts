ccd3e28eab50d4c1fb84502517e35c0285c964d7
Zach <100164907@ccisd.net>
2025-10-29 16:43:52 -0500

    Create Text Colored.lua

---
+++ Text Colored.lua
@@ -0,0 +1,21 @@
+local RESET = "\033[0m"
+local BLACK = "\033[30m"
+local RED = "\033[31m"
+local GREEN = "\033[32m"
+local YELLOW = "\033[33m"
+local BLUE = "\033[34m"
+local MEGENTA = "\033[35m"
+local CYAN = "\033[36m"
+local WHITE = "\033[37m"
+local BOLDBLACK = "\033[1m\033[30m"
+local BOLDRED = "\033[1m\033[31m"
+local BOLDGREEN = "\033[1m\033[32m"
+local BOLDYELLOW = "\033[1m\033[33m"
+local BOLDBLUE = "\033[1m\033[34m"
+local BOLDMEGENTA = "\033[1m\033[35m"
+local BOLDCYAN = "\033[1m\033[36m"
+local BOLDWHITE = "\033[1m\033[37m"
+
+-- Example
+
+print(RED .. "Hello!")
